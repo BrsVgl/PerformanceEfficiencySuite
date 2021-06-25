f55c5ac676c056df6b6061fd13e82cef7fb87c23
Boris Vogel <boris.vogel.82@gmail.com>
2021-06-25 18:42:12 +0200

    - Refactoring: function for measurement - some basic exception handling when given wrong CB23 path

--- Ressources/Main.ps1
+++ Ressources/Main.ps1
@@ -1,6 +1,9 @@
 #Requires -RunAsAdministrator
 Clear-Host
 
+# declare CSV header
+$strCsvHeader = "DurationMilliseconds;PackagePower`n"
+
 # get path of "Ressources" folder
 $strPesPath = $MyInvocation.MyCommand.Path
 $strPesPath = $strPesPath.Replace($MyInvocation.MyCommand.Name, '') 
@@ -16,9 +19,35 @@ Get-Content  $strSettingsPath | foreach-object -begin {$htSettings=@{}} -process
 		} 
 	}
 
+# declare function Get-PackagePower
+function Get-CpuPackagePower {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory)]
+		$prcProcessToMonitor,
+		$strHeader
+	)
+	$strResult = $strHeader
+	$dtInitial = Get-Date
+	While (!$prcProcessToMonitor.HasExited) {
+		$varHW.Update()
+		ForEach ($varSensor in $varHW.Sensors) {
+			If (!(($varSensor.SensorType -eq 'Power') -and ($varSensor.Name -eq 'CPU Package'))) {continue}             
+			$PackagePower = $varSensor.Value
+		}
+		$tsDuration = New-TimeSpan -Start $dtInitial -End (Get-Date)
+		$decDurationMilliSeconds = $tsDuration.TotalMilliseconds
+		$strResult += "$decDurationMilliSeconds;$PackagePower`n"		
+		Start-Sleep -Milliseconds 10
+	}
+	$strResult = $strResult.Substring(0, $strResult.Length - 1)
+	# return value
+	$strResult
+}
+
 # set CinemaBenchR23 Path
 $strCb23Path = $htSettings['Cb23Path']
-$strCb23Path += '\Cinebench.exe'
+$strCb23Executable = $strCb23Path + '\Cinebench.exe'
 
 # set LogCsv Path
 $strLogCsvPath = $strPesPath.Replace('Ressources\', '') + 'LogCsv\'
@@ -33,82 +62,58 @@ $cComp = New-Object -TypeName OpenHardwareMonitor.Hardware.Computer
 $cComp.Open()
 # there are more components available; we just need "CPU"
 $cComp.CPUEnabled = $true
-
-Write-Output "[$(Get-Date -format 'u')] Starting CB23 Single-Thread test..."
+$varHW = $cComp.Hardware
 
 # start CB R23 single-thread mode and monitor its process
-Start-Process -FilePath $strCb23Path -ArgumentList "g_CinebenchCpu1Test=true g_CinebenchMinimumTestDuration=1"
+Write-Output "[$(Get-Date -format 'u')] Starting CB23 Single-Thread test..."
+try {
+	Start-Process -FilePath $strCb23Executable -ArgumentList "g_CinebenchCpu1Test=true g_CinebenchMinimumTestDuration=1"
+}
+
+catch {
+	Write-Output '--------------------------------------------------------------------------------------------------------------'
+	Write-Output '| ERROR: Cinebench R23 could not be started...'
+	Write-Output '| HINT:  Cinebench R23 path in Settings.txt:' 
+	Write-Output "|        $strCb23Path"
+	Write-Output '|        Are you sure this is correct?'
+	Write-Output '--------------------------------------------------------------------------------------------------------------'
+
+	# dispose Object
+	# $cComp.Close()
+	Write-Output 'Inside catch'
+	Start-Sleep -Seconds 30
+	break
+}
 $prcCinebench = Get-Process Cinebench
 $prcCinebench.PriorityClass = "AboveNormal"
 
 # start measurement
-$dtInitial = Get-Date
-$strResult = "DurationMilliseconds;PackagePower`n"
-
-While (!$prcCinebench.HasExited) {
-	ForEach ($varHW in $cComp.Hardware) {
-		If (!$varHW.HardwareType -eq "CPU"){continue}
-		# needed to keep Processor load of the measurement at bay
-		Start-Sleep -Milliseconds 10
-		$varHW.Update()
-		ForEach ($varSensor in $varHW.Sensors) {
-			If (!(($varSensor.SensorType -eq 'Power') -and ($varSensor.Name -eq 'CPU Package'))) {continue}             
-			$PackagePower = $varSensor.Value
-		}
-		$tsDuration = New-TimeSpan -Start $dtInitial -End (Get-Date)
-		$decDurationMilliSeconds = $tsDuration.TotalMilliseconds
-		$strResult += "$decDurationMilliSeconds;$PackagePower`n"		
-	}
-}
+$strResult = Get-CpuPackagePower -prcProcessToMonitor $prcCinebench -strHeader $strCsvHeader
 Write-Output "[$(Get-Date -format 'u')] CB23 ST duration: $decDurationMilliSeconds ms"
-# just cut the last line-break for cleanliness
-$strResult = $strResult.Substring(0, $strResult.Length - 1)
-$strCb23StCsv = $strLogCsvPath + 'Cb23St.csv'
+
 # dump data to CSV
+$strCb23StCsv = $strLogCsvPath + 'Cb23St.csv'
 Write-Output $strResult | Out-File -Filepath $strCb23StCsv
 
 # Cooldown
-# Write-Output ""
-# Write-Output "[$(Get-Date -format 'u')] 180 seconds cooldown remaining until Multi-Thread test..."
-# Start-Sleep -Seconds 60
-# Write-Output "[$(Get-Date -format 'u')] 120 seconds cooldown remaining until Multi-Thread test..."
-# Start-Sleep -Seconds 60
-# Write-Output "[$(Get-Date -format 'u')] 60 seconds cooldown remaining until Multi-Thread test..."
-# Start-Sleep -Seconds 50
 Write-Output "[$(Get-Date -format 'u')] 10 seconds cooldown remaining until Multi-Thread test..."
 Start-Sleep -Seconds 10
-Write-Output ""
 
 # the same as above - just for multi-threaded run
 Write-Output "[$(Get-Date -format 'u')] Starting CB23 Multi-Thread test..."
-
-Start-Process -FilePath $strCb23Path -ArgumentList "g_CinebenchCpuXTest=true g_CinebenchMinimumTestDuration=1"
+Start-Process -FilePath $strCb23Executable -ArgumentList "g_CinebenchCpuXTest=true g_CinebenchMinimumTestDuration=1"
 $prcCinebench = Get-Process Cinebench
 $prcCinebench.PriorityClass = "AboveNormal"
 
-$dtInitial = Get-Date
-$strResult = "DurationMilliseconds;PackagePower`n"
+$strResult = Get-CpuPackagePower -prcProcessToMonitor $prcCinebench -strHeader $strCsvHeader
+Write-Output "[$(Get-Date -format 'u')] CB23 Multi-Thread duration: $decDurationMilliSeconds ms"
+$strCb23MtCsv = $strLogCsvPath + 'Cb23Mt.csv'
+Write-Output $strResult | Out-File -Filepath $strCb23MtCsv
 
-While (!$prcCinebench.HasExited) {
-	# ForEach ($varHW in $cComp.Hardware) {
-		# "varHW" #DEBUG
-		# '-----' #DEBUG
-		# $varHW #DEBUG
-		# If (!$varHW.HardwareType -eq "CPU"){continue}
-		Start-Sleep -Milliseconds 5
-		$varHW.Update()
-		ForEach ($varSensor in $varHW.Sensors) {
-			# "varSensor" #DEBUG
-			# '-----' #DEBUG
-			# $varSensor #DEBUG
-			If (!(($varSensor.SensorType -eq 'Power') -and ($varSensor.Name -eq 'CPU Package'))) {continue}             
-			$PackagePower = $varSensor.Value
-		}
-		$tsDuration = New-TimeSpan -Start $dtInitial -End (Get-Date)
-		$decDurationMilliSeconds = $tsDuration.TotalMilliseconds
-		$strResult += "$decDurationMilliSeconds;$PackagePower`n"		
-	}
-# }
+$cComp.Close()
+"[$(Get-Date -format 'u')] Finished... Closing window in 10 seconds"
+Start-Sleep -Seconds 10
+
 
 ## DEBUG Begin ########################################################################################################
 # "strSettingsPath: $strSettingsPath"
@@ -118,12 +123,3 @@ While (!$prcCinebench.HasExited) {
 # Start-Sleep -Seconds 1000
 # break
 ## DEBUG End ##########################################################################################################
-
-Write-Output "[$(Get-Date -format 'u')] CB23 Multi-Thread duration: $decDurationMilliSeconds ms"
-$strResult = $strResult.Substring(0, $strResult.Length - 1)
-$strCb23MtCsv = $strLogCsvPath + 'Cb23Mt.csv'
-Write-Output $strResult | Out-File -Filepath $strCb23MtCsv
-
-$cComp.Close()
-"[$(Get-Date -format 'u')] Finished... Closing window in 10 seconds"
-Start-Sleep -Seconds 10
